03cfbfe28e21deeb0b42482b2862905873637ab3
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-21 20:41:07 -0300

    :sparkles: create: fields - Bolo

--- Samuca-s_Bakery/src/main/java/com/bakery/model/Bolo.java
+++ Samuca-s_Bakery/src/main/java/com/bakery/model/Bolo.java
@@ -2,6 +2,16 @@ package com.bakery.model;
 
 // Forma/molde de Bolo ( ingredientes e preparativos )
 public class Bolo {
+    private Sabor sabor;
+    private Cobertura cobertura;
+    private final float peso;
+    private float quantidade;
+    //private Massa massa;
+
+    public Bolo(){
+        peso = 1;
+    }
+
 
 
     public enum Sabor {
