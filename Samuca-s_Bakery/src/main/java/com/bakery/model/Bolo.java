6e941b97c22ae1d288beb611e30bc8eb836ff122
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-21 20:38:30 -0300

    :sparkles: create: enum - sabor (bolo)

--- Samuca-s_Bakery/src/main/java/com/bakery/model/Bolo.java
+++ Samuca-s_Bakery/src/main/java/com/bakery/model/Bolo.java
@@ -3,5 +3,16 @@ package com.bakery.model;
 // Forma/molde de Bolo ( ingredientes e preparativos )
 public class Bolo {
 
+
+    public enum Sabor {
+        Morango("Morango"),
+        Chocolate("Chocolate"),
+        Cenoura("Cenoura");
+
+        String flavor;
+
+        private Sabor(String flavor){ this.flavor = flavor; }
+
+    }
 }
 
