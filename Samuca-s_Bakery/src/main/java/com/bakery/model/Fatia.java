249280f993acb9b95934527f17e8c47ad84e205a
samuel-ricardo <samuelricardoofficial@gmail.com>
2022-09-21 21:19:04 -0300

    :sparkles: create: Enum - Tamanho (Fatia)

--- Samuca-s_Bakery/src/main/java/com/bakery/model/Fatia.java
+++ Samuca-s_Bakery/src/main/java/com/bakery/model/Fatia.java
@@ -6,4 +6,26 @@ public class Fatia {
     private Tamanho tamanho;
 
 
+
+    public enum Tamanho {
+        GRANDE(0.250, "Grande"),
+        MEDIA(0.200, "Media"),
+        PEQUENA(0.150, "Pequena");
+
+        double peso;
+        String name;
+
+        private Tamanho (double peso, String name){
+            this.peso = peso;
+            this.name = name;
+        }
+
+        public double getPeso() {
+            return peso;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
